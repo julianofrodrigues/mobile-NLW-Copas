3f4c4ab9118800a626dd5c6d278c6ad10d5ce680
Juliano Ferreira Rodrigues <julianof.rodrigues@hotmail.com>
2022-11-04 00:05:52 -0300

    components + auth/hooks + news screens

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,12 +1,18 @@
-import { createContext, ReactNode } from "react";
+import { createContext, ReactNode, useEffect, useState } from "react";
+import * as Google from 'expo-auth-session/providers/google'
+import * as AuthSession from 'expo-auth-session';
+import * as WebBrowser from 'expo-web-browser';
+
+WebBrowser.maybeCompleteAuthSession();
 
 interface UserProps{
     name: string;
-    avatarUrl: string
+    avatarUrl: string,
 }
 
 export interface AuthContextDataProps {
     user: UserProps;
+    isUserLoading: boolean,
     signIn: () => Promise<void>
 }
 
@@ -17,16 +23,44 @@ interface AuthProviderProps {
 export const AuthContext = createContext({} as AuthContextDataProps);
 
 export function AuthContextProvider({ children } : AuthProviderProps){
+    const [user, setUser] = useState<UserProps>({} as UserProps);
+    const [isUserLoading, setIsUserLoading] = useState(false);
+
+    const [request, response, promptAsync] = Google.useAuthRequest({
+        clientId: '220096647366-oqe840i5lsh2gdgm2q9p6up84vaak85o.apps.googleusercontent.com',
+        redirectUri: AuthSession.makeRedirectUri({ useProxy: true }),
+        scopes: ['profile', 'email']
+    })
+
+    // console.log(AuthSession.makeRedirectUri({ useProxy: true })) pega redirecionamento autorizado
+
     async function signIn() {
-        console.log('logou!')
+        try {
+            setIsUserLoading(true)
+            await promptAsync();
+        } catch (error) {
+            console.log(error);
+            throw error;
+        } finally {
+            setIsUserLoading(false)
+        }
     }
+
+    async function signInWithGoogle(access_token: string) {
+        console.log('token =>', access_token)
+    }
+
+    useEffect(() =>{
+        if(response?.type === 'success' && response.authentication?.accessToken){
+            signInWithGoogle(response.authentication.accessToken)
+        }
+    }, [response])
+
     return(
         <AuthContext.Provider value={{
             signIn,
-            user: {
-                name: 'Juliano',
-                avatarUrl: 'https://avatars.githubusercontent.com/u/46505899?v=4'
-            }
+            isUserLoading,
+            user,
         }}>
             { children }
         </AuthContext.Provider>
