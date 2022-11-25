6983bec4963e60b7d98373fa494354b262e1468e
unknown <julianof.rodrigues@hotmail.com>
2022-11-25 17:17:14 -0300

    init details

--- src/screens/Pools.tsx
+++ src/screens/Pools.tsx
@@ -49,7 +49,12 @@ export function Pools(){
                 <FlatList 
                     data={pools}
                     keyExtractor={item => item.id}
-                    renderItem={({item}) => <PoolCard data={item} />}
+                    renderItem={({item}) => (
+                        <PoolCard 
+                            data={item}
+                            onPress={() => navigate('details', { id: item.id })}
+                        />
+                    )}
                     px={5}
                     showsHorizontalScrollIndicator={false}
                     _contentContainerStyle={{pb: 10}}
